ea2474c75177f5b7b5350c2eced66c60e898b0b9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-31 01:05:01 -0700

    Initial Commit with simple, dumb, list everything tab

---
+++ Source/List_Everything.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Harmony;
+
+namespace List_Everything
+{
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+#if DEBUG
+			HarmonyInstance.DEBUG = true;
+#endif
+
+			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.List_Everything.main");
+			
+			harmony.PatchAll();
+		}
+	}
+}
---
+++ Source/MainTabWindow_List.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace List_Everything
+{
+	public class MainTabWindow_List : MainTabWindow
+	{
+		private Vector2 scrollPosition = Vector2.zero;
+
+		private float scrollViewHeight;
+
+		public override void DoWindowContents(Rect fillRect)
+		{
+			base.DoWindowContents(fillRect);
+			GUI.BeginGroup(fillRect);
+			GUI.color = Color.white;
+			
+			Rect viewRect = new Rect(0f, 0f, fillRect.width - 16f, scrollViewHeight);
+			Widgets.BeginScrollView(fillRect, ref scrollPosition, viewRect);
+			float totalHeight = 0f;
+
+			foreach (Thing thing in Find.CurrentMap.listerThings.AllThings)
+			{
+				if (!thing.Fogged() || DebugSettings.godMode)
+				{
+					 DrawThingRow(thing, ref totalHeight, viewRect);
+				}
+			}
+
+			if (Event.current.type == EventType.Layout)
+				scrollViewHeight = totalHeight;
+			Widgets.EndScrollView();
+			GUI.EndGroup();
+		}
+
+		private static void DrawThingRow(Thing thing, ref float rowY, Rect fillRect)
+		{
+			Rect rect = new Rect(fillRect.x, rowY, fillRect.width, 32);
+			rowY += 34;
+			Rect iconRect = rect.LeftPartPixels(32);
+			Rect labelRect = new Rect(rect.x + 34, rect.y, rect.width - 34, rect.height);
+
+			Widgets.ThingIcon(iconRect, thing);
+			Widgets.Label(labelRect, thing.LabelCap);
+		}
+	}
+}
