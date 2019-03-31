c0a8d51bec8bc8d3742e028356e4b23d1b8acce2
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-31 04:35:11 -0700

    Method for DrawThing for later, harder-to-draw things

--- Source/MainTabWindow_List.cs
+++ Source/MainTabWindow_List.cs
@@ -36,6 +36,8 @@ namespace List_Everything
 			DoList(listRect);
 		}
 
+		//Lists:
+
 		//public ListerThings listerThings;
 		bool listByName;
 		string listByNameStr = "";
@@ -57,6 +59,10 @@ namespace List_Everything
 		//public ListerFilthInHomeArea listerFilthInHomeArea;
 		bool listFilth;
 
+		//Filters:
+
+		//bool? filterForbidden;
+
 		public void DoFilter(Rect rect)
 		{
 			Listing_Standard listing = new Listing_Standard();
@@ -71,6 +77,8 @@ namespace List_Everything
 			listing.CheckboxLabeled("Things Needing Hauling", ref listHaulable);
 			listing.CheckboxLabeled("Stacks Needing Merging", ref listMergable);
 			listing.CheckboxLabeled("Filth in Home Area", ref listFilth);
+			//listing.GapLine();
+			//listing.CheckboxLabeledSelectable("Forbidden", ref listFilth);
 
 			listing.End();
 		}
@@ -140,15 +148,13 @@ namespace List_Everything
 		{
 			Rect rect = new Rect(fillRect.x, rowY, fillRect.width, 32);
 			rowY += 34;
-			
+
 			//Highlight selected
 			if (Find.Selector.IsSelected(thing))
 				Widgets.DrawHighlightSelected(rect);
 
 			//Draw
-			Rect iconRect = rect.RightPartPixels(32 * (thing.def.graphicData?.drawSize.x / thing.def.graphicData?.drawSize.y ?? 1));
-			Widgets.ThingIcon(iconRect, thing);
-			Widgets.Label(rect, thing.LabelCap);
+			DrawThing(rect, thing);
 
 			//Draw arrow pointing to hovered thing
 			if (Mouse.IsOver(rect))
@@ -223,6 +229,17 @@ namespace List_Everything
 						Find.Selector.Deselect(thing);
 				}
 			}
+		}
+
+		public static void DrawThing(Rect rect, Thing thing)
+		{
+			//Label
+			Widgets.Label(rect, thing.LabelCap);
+
+
+			//Icon
+			Rect iconRect = rect.RightPartPixels(32 * (thing.def.graphicData?.drawSize.x / thing.def.graphicData?.drawSize.y ?? 1));
+			Widgets.ThingIcon(iconRect, thing);
 		}
 	}
 }
