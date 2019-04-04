1ab808089f4c74d1505bdba724ba05bc5b590fa5
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-04-04 12:06:01 -0700

    Empty Alert, todo: make them and add to alert list.

---
+++ Source/AlertByFind.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using static System.Reflection.BindingFlags;
+using Verse;
+using RimWorld;
+
+namespace List_Everything
+{
+	public static class AlertByFind
+	{
+		public static FieldInfo AllAlertsInfo = typeof(AlertsReadout).GetField("AllAlerts", NonPublic | Instance);
+		public static List<Alert> AllAlerts
+		{
+			get
+			{
+				if ((Find.UIRoot as UIRoot_Play).alerts is AlertsReadout readout)
+					return (List<Alert>)AllAlertsInfo.GetValue(readout);
+				return null;
+			}
+		}
+	}
+
+	public class Alert_Find : Alert
+	{
+		public FindDescription filter;
+
+		public Alert_Find()
+		{
+			//The vanilla alert added to AllAlerts will be constructed but never trigger
+			this.defaultLabel = "Alert!";
+			this.defaultPriority = AlertPriority.Medium;
+		}
+
+		public Alert_Find(FindDescription f) : this()
+		{
+			filter = f;
+		}
+
+		public override AlertReport GetReport()
+		{
+			return filter == null ? AlertReport.Inactive : 
+				AlertReport.CulpritsAre(FoundThings());
+		}
+
+		public override string GetExplanation()
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine("Your custom alert:");
+			stringBuilder.AppendLine("");
+			foreach (Thing thing in this.FoundThings())
+				stringBuilder.AppendLine("   " + thing.Label);
+
+			return stringBuilder.ToString().TrimEndNewlines();
+		}
+
+		private IEnumerable<Thing> FoundThings()
+		{
+			foreach (Map map in Find.Maps.Where(m => m.IsPlayerHome))
+				foreach (Thing t in filter.Get(map))
+					yield return t;
+		}
+	}
+}
