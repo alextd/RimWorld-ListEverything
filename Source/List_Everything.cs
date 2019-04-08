8cfba5f5b212da78e116e52e0b4939f58643651a
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-04-08 02:33:51 -0700

    I don't even understand how I didn't remember to delete Harmony

--- Source/List_Everything.cs
+++ Source/List_Everything.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Verse;
 using RimWorld;
 using UnityEngine;
-using Harmony;
 
 namespace List_Everything
 {
@@ -11,13 +10,6 @@ namespace List_Everything
 	{
 		public Mod(ModContentPack content) : base(content)
 		{
-#if DEBUG
-			HarmonyInstance.DEBUG = true;
-#endif
-
-			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.List_Everything.main");
-			
-			harmony.PatchAll();
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect)
