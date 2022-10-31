263956f3b79b84ab1ca7f7db2ca16e7ae78d1291
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-10-31 13:33:27 -0700

    Multi-group reordering, so the any/all filters can reorder to the mainlist and back
    Gather up the reorderIDs of any nested groups.
    
    Override vanilla code that messes with groups based on their rects, (absRect doesn't work with overlapping.)
    
    Smarter use of full height rects : set yMax instead of computing height based on differences.

--- Source/FilterOwner.cs
+++ Source/FilterOwner.cs
@@ -62,6 +62,86 @@ namespace List_Everything
 			if (remake) parent.RootFindDesc.RemakeList();
 		}
 
+		//Gather method that passes in both FindDescription and all ListFilters to selector
+		public IEnumerable<T> Gather<T>(Func<object, T?> selector) where T : struct
+		{
+			{
+				if (selector(parent) is T result)
+					yield return result;
+			}
+			foreach (var filter in filters)
+			{
+				{
+					if (selector(filter) is T result)
+						yield return result;
+				}
+				if (filter is IFilterHolder childHolder)
+					foreach (T r in childHolder.Children.Gather(selector))
+						yield return r;
+			}
+		}
+		//sadly 100% copied, subtract the "?" oh gee.
+		public IEnumerable<T> Gather<T>(Func<object, T> selector) where T : class
+		{
+			{
+				if (selector(parent) is T result)
+					yield return result;
+			}
+			foreach (var filter in filters)
+			{
+				{
+					if (selector(filter) is T result)
+						yield return result;
+				}
+				if (filter is IFilterHolder childHolder)
+					foreach (T r in childHolder.Children.Gather(selector))
+						yield return r;
+			}
+		}
+
+		//Gather method that passes in both FindDescription and all ListFilters to selector
+		public void ForEach(Action<object> action)
+		{
+			action(parent);
+			foreach (var filter in filters)
+			{
+				if (filter is IFilterHolder childHolder)
+					childHolder.Children.ForEach(action);	//handles calling on itself
+				else //just a filter then
+					action(filter);
+			}
+		}
+
+		public void MasterReorder(int from, int fromGroup, int to, int toGroup)
+		{
+			Log.Message($"MasterReorder(int from={from}, int fromGroup={fromGroup}, int to={to}, int toGroup={toGroup})");
+
+			ListFilter draggedFilter = Gather(delegate (object o)
+			{
+				if (o is IFilterHolder holder)
+				{
+					if (holder.Children.reorderID == fromGroup)
+					{
+						ListFilter result = holder.Children.filters.ElementAt(from);
+						holder.Children.filters.RemoveAt(from);
+						return result;
+					}
+				}
+				return null;
+			}).First();
+
+			ForEach(delegate (object o)
+			{
+				if (o is IFilterHolder holder)
+				{
+					if (holder.Children.reorderID == toGroup)
+					{
+						holder.Children.filters.Insert(to, draggedFilter);
+					}
+				}
+			});
+		}
+
 		//Draw filters completely, in a rect
 		private Vector2 scrollPositionFilt = Vector2.zero;
 		private float scrollHeight;
@@ -80,6 +160,10 @@ namespace List_Everything
 
 			bool changed = DrawFilters(listing, locked);
 
+			List<int> reorderIDs = new(Gather(f => (f as IFilterHolder)?.Children.reorderID));
+
+			ReorderableWidget.NewMultiGroup(reorderIDs, parent.RootFindDesc.Children.MasterReorder);
+
 			listing.EndScrollView(ref scrollHeight);
 
 			return changed;
@@ -87,15 +171,12 @@ namespace List_Everything
 
 
 		// draw filters continuing a Listing_StandardIndent
-		private int reorderID;
+		public int reorderID;
 		private float reorderRectHeight;
 
 		public bool DrawFilters(Listing_StandardIndent listing, bool locked)
 		{
-			float heightBeforeAll = listing.CurHeight;
-
-
-			Rect coveredRect = new Rect(0f, heightBeforeAll, listing.ColumnWidth, reorderRectHeight);
+			Rect coveredRect = new Rect(0f, listing.CurHeight, listing.ColumnWidth, reorderRectHeight);
 			if (Event.current.type == EventType.Repaint)
 			{
 				reorderID = ReorderableWidget.NewGroup(
@@ -104,6 +185,13 @@ namespace List_Everything
 					coveredRect, 1f,
 					extraDraggedItemOnGUI: (int index, Vector2 dragStartPos) =>
 						DrawMouseAttachedFilter(Filters.ElementAt(index), coveredRect.width - 100));
+
+				// Turn off The Multigroup system assuming that if you're closer to group A but in group B's rect, that you want to insert at end of B.
+				// That just doesn't apply here.
+				// (it uses absRect to check mouseover group B and that overrides if you're in an okay place to drop in group A)
+				var group = ReorderableWidget.groups[reorderID];	//immutable struct O_o
+				group.absRect = new Rect();
+				ReorderableWidget.groups[reorderID] = group;
 			}
 
 			bool changed = false;
@@ -111,7 +199,7 @@ namespace List_Everything
 			foreach (ListFilter filter in Filters)
 			{
 				Rect usedRect = listing.GetRect(0);
-				float heightBeforeSingle = listing.CurHeight;
+
 				(bool ch, bool d) = filter.Listing(listing, locked);
 				changed |= ch;
 				if (d)
@@ -124,12 +212,12 @@ namespace List_Everything
 				// Highlight the filters that pass for selected objects (useful for "any" filters)
 				if (!(filter is IFilterHolder) && Find.Selector.SelectedObjects.Any(o => o is Thing t && filter.AppliesTo(t)))
 				{
-					usedRect.height = listing.CurHeight - heightBeforeSingle;
+					usedRect.yMax = listing.CurHeight;
 					Widgets.DrawHighlight(usedRect);
 				}
 			}
 
-			reorderRectHeight = listing.CurHeight - heightBeforeAll;
+			reorderRectHeight = listing.CurHeight - coveredRect.y;
 
 			RemoveAll(removedFilters);
 
