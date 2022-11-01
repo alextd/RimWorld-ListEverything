64f08173bbfc8c470d29a63e7ffcfb871f975c06
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-10-31 22:01:32 -0700

    ThingDef filter include stack size.

--- Source/ListFilter.cs
+++ Source/ListFilter.cs
@@ -971,13 +971,33 @@ namespace List_Everything
 
 	class ListFilterThingDef : ListFilterDropDown<ThingDef>
 	{
+		public IntRange stackRange;
 		public ListFilterThingDef()
 		{
 			sel = ThingDefOf.WoodLog;
 		}
+		protected override void PostSelected()
+		{
+			stackRange.min = 1;
+			stackRange.max = sel.stackLimit;
+		}
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref stackRange, "stackRange");
+		}
+		public override ListFilter Clone()
+		{
+			ListFilterThingDef clone = (ListFilterThingDef)base.Clone();
+			clone.stackRange = stackRange;
+			return clone;
+		}
+
 
 		protected override bool FilterApplies(Thing thing) =>
-			sel == thing.def;
+			sel == thing.def &&
+			(sel.stackLimit <= 1 || stackRange.Includes(thing.stackCount));
 
 		public override IEnumerable<ThingDef> Options() =>
 			(ContentsUtility.OnlyAvailable ?
@@ -986,6 +1006,21 @@ namespace List_Everything
 			.Where(def => FindDescription.ValidDef(def));
 
 		public override bool Ordered => true;
+
+		public override bool DrawCustom(Rect rect, WidgetRow row)
+		{
+			if (sel.stackLimit > 1)
+			{
+				IntRange newRange = stackRange;
+				Widgets.IntRange(rect, id, ref newRange, 1, sel.stackLimit);
+				if (newRange != stackRange)
+				{
+					stackRange = newRange;
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 
 	class ListFilterModded : ListFilterDropDown<ModContentPack>
