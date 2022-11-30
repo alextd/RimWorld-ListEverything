4926defc447338525332a859e72573accfd932ec
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-29 18:46:23 -0800

    For 1.4, Just pop up a message to use Ctrl-F or Custom Alerts.

--- Source/List_Everything.cs
+++ Source/List_Everything.cs
@@ -7,21 +7,14 @@ namespace List_Everything
 {
 	public class Mod : Verse.Mod
 	{
-		public static Settings settings;
 		public Mod(ModContentPack content) : base(content)
 		{
-			LongEventHandler.ExecuteWhenFinished(() => { settings = GetSettings<Settings>(); });
-		}
+			LongEventHandler.ExecuteWhenFinished(() => {
+				Log.Warning($"Hey! List Everything has been turned into the mods Ctrl-F and Custom Alerts for 1.4. Go use those!");
 
-		public override void DoSettingsWindowContents(Rect inRect)
-		{
-			base.DoSettingsWindowContents(inRect);
-			settings.DoWindowContents(inRect);
-		}
-
-		public override string SettingsCategory()
-		{
-			return "TD.ListEverything".Translate();
+				Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation(
+					"Hey! List Everything has been turned into the mods Ctrl-F and Custom Alerts for 1.4. Go use those!", () => { }));
+			});
 		}
 	}
 }
